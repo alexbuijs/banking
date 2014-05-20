f4e190747c9ced4309f33655b02b0559598664a2
Alex Buijs <contact@alexbuijsweb.nl>
2014-05-20 20:13:47 +0200

    add devise and run generator

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.4'
 gem 'haml'
 gem 'twitter-bootstrap-rails'
+gem 'devise'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Banking::Application.routes.draw do
+  devise_for :users
+  root to: 'home#index'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
